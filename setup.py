4720b2cb8b8d93814ee5bbe7e1984093aab25f65
bhattadeb34 <106618568+bhattadeb34@users.noreply.github.com>
2025-09-28 12:58:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pm7calculator",
     version="0.1.0",
-    author="Your Name", 
+    author="Debjyoti Bhattacharya", 
     description="Simple PM7 calculator for Google Colab",
     packages=find_packages(),
     install_requires=[
